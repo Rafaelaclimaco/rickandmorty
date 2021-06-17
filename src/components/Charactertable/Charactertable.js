ae5d1f243011ec71eb60adb4649fac11b9db8057
Rafaela Clímaco <72920150+Rafaelaclimaco@users.noreply.github.com>
2021-06-17 17:37:46 +0100

    1erro

--- src/components/Charactertable/Charactertable.js
+++ src/components/Charactertable/Charactertable.js
@@ -1,21 +1,21 @@
 import styles from './Charactertable.module.scss';
 import Link from 'next/link';
-import { Code } from '@material-ui/icons';
+
 
 
 export default function Charactertable({ nome, especie, planeta, image, code }) {
   return (
+  
+  <a>
     <Link href={`character/${code}`}>
-      <a>
         <div className={styles.charactherContainer}>
-
           <div className={styles.conteinerimg}><img height="100" src={image} /> </div>
           <div className={styles.nome}>{nome}</div>
           <div className={styles.planeta}>{planeta}</div>
           <div className={styles.especie}>{especie}</div>
         </div>
-      </a>
     </Link>
+     </a>
   )
 }
 
